78be5aee05c419ceebcfce9ab0e039d4af59e110
jnam <onik.1899@gmail.com>
2021-03-17 15:25:07 -0500

    firebase, AngularFire

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-
+import { AngularFireModule } from '@angular/fire'
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { AngularFireStorageModule } from '@angular/fire/storage';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, AngularFireModule.initializeApp(environment.firebaseConfig), AngularFirestoreModule, AngularFireAuthModule, AngularFireDatabaseModule, AngularFireStorageModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyBo-LbH2YeLhMfuVp55jhrICPsKHXSQH3Y",
+    authDomain: "delcampacol-colombia.firebaseapp.com",
+    databaseURL: "https://delcampacol-colombia-default-rtdb.firebaseio.com",
+    projectId: "delcampacol-colombia",
+    storageBucket: "delcampacol-colombia.appspot.com",
+    messagingSenderId: "107745587353",
+    appId: "1:107745587353:web:b4cad4b6a98add18094808"
+  }
 };
 
 /*
