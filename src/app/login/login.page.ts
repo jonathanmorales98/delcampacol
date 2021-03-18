e511e83e19afe704f349792813fa84d832f1b6fc
jnam <onik.1899@gmail.com>
2021-03-18 18:05:13 -0500

    pequeños cambios para la BD

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFireDatabase } from '@angular/fire/database';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  itemRef: any;
+
+  loginForm: FormGroup
+
+  constructor(private db: AngularFireDatabase, private auth:AngularFireAuth, private fb:FormBuilder) { }
 
   ngOnInit() {
+    this.itemRef = this.db.object('item');
+    this.itemRef.snapshotChanges().subscribe(action => {
+      console.log(action.type);
+      console.log(action.key)
+      console.log(action.payload.val())
+    });
+
+    this.loginForm = this.fb.group(
+      {
+        email:["",Validators.required],
+        pass:["",Validators.required]
+      }
+    )
+  }
+
+  login()
+  {
+    
   }
 
 }
--- src/app/mercado/mercado.page.ts
+++ src/app/mercado/mercado.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/database';
 
 @Component({
   selector: 'app-mercado',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MercadoPage implements OnInit {
 
-  constructor() { }
+  itemRef: any;
+
+  constructor(private db: AngularFireDatabase) { }
 
   ngOnInit() {
+    this.itemRef = this.db.object('Oferta');
+    this.itemRef.snapshotChanges().subscribe(action => {
+      console.log(action.type);
+      console.log(action.key)
+      console.log(action.payload.val())
+    });
   }
 
 }
