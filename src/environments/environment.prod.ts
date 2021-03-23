f7c237d434eb509c41337edd410bb829d4173895
jonathanmorales98 <jonathanmorbon@gmail.com>
2021-03-22 21:39:35 -0500

    firebaseconfig

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig :{
+    apiKey: "AIzaSyBo-LbH2YeLhMfuVp55jhrICPsKHXSQH3Y",
+    authDomain: "delcampacol-colombia.firebaseapp.com",
+    databaseURL: "https://delcampacol-colombia-default-rtdb.firebaseio.com",
+    projectId: "delcampacol-colombia",
+    storageBucket: "delcampacol-colombia.appspot.com",
+    messagingSenderId: "107745587353",
+    appId: "1:107745587353:web:b4cad4b6a98add18094808"
+  },
 };
