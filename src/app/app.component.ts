b700b2a7926704ecb0d00197772cab02dd4ecda7
jnam <onik.1899@gmail.com>
2021-03-24 18:46:27 -0500

    importa auth y verificacion del estado del usuario para redireccionar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,25 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor(private auth:AngularFireAuth, private router:Router) 
+  {
+    this.verifyCurrentUser()
+  }
+
+  verifyCurrentUser(){
+    this.auth.authState.subscribe(e=>
+      {
+        console.log(e)
+        if(e == null)
+        {
+          this.router.navigate(['/'])
+        }
+        else
+        {
+          this.router.navigate(['/tabs/noticias'], {replaceUrl:true}) //el replace es para que no se regrese con las flechas del celular
+        }
+        
+      })
+  }
 }
+
