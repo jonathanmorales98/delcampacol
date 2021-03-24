c7d31a724a53d1f838c82a22406077d7d64d5c77
jnam <onik.1899@gmail.com>
2021-03-24 18:46:47 -0500

    se importan  modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,11 +11,23 @@ import { AngularFireStorageModule } from '@angular/fire/storage';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { environment } from 'src/environments/environment';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, AngularFireModule.initializeApp(environment.firebaseConfig), AngularFirestoreModule, AngularFireAuthModule, AngularFireDatabaseModule, AngularFireStorageModule],
+  imports: [
+    BrowserModule, 
+    IonicModule.forRoot(), 
+    AppRoutingModule,
+    ReactiveFormsModule, //se intancian estas dos para que funcione los formularios
+    FormsModule,        //2
+    AngularFireModule.initializeApp(environment.firebaseConfig), 
+    AngularFirestoreModule, 
+    AngularFireAuthModule, 
+    AngularFireDatabaseModule, 
+    AngularFireStorageModule
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
