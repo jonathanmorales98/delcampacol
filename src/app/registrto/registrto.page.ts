5809671e3aec44b3e91282d2af236e4e9c4068df
jnam <onik.1899@gmail.com>
2021-03-24 18:49:46 -0500

    funcion register() para el registro, una funcion alerta y conexion a la base de datos

--- src/app/registrto/registrto.page.ts
+++ src/app/registrto/registrto.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFireDatabase } from '@angular/fire/database';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-registrto',
@@ -7,9 +11,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrtoPage implements OnInit {
 
-  constructor() { }
+  constructor(private auth:AngularFireAuth, private fb:FormBuilder, private alertController:AlertController, private db:AngularFireDatabase) { }
+
+  regsiterForm: FormGroup;
 
   ngOnInit() {
+    this.regsiterForm = this.fb.group(
+      {
+        name:['', Validators.required],
+        lastname:['', Validators.required],
+        number:['', Validators.required],
+        email:['', Validators.required],
+        pass:['', Validators.required],
+        conPass:['', Validators.required],
+        type:['', Validators.required],
+      }
+    )
+  }
+
+  register()
+  {
+    let user = 
+    {
+      email: this.regsiterForm.controls['email'].value,
+      password: this.regsiterForm.controls['password'].value
+    }
+    if(user.password == this.regsiterForm.controls['conPass'].value)
+    {
+      this.auth.createUserWithEmailAndPassword(user.email, user.password).then(userData=>
+        {
+          this.registerAlert('Correcto', "El usuario ha sido creado correctamente")
+          //this.db.database.ref('Users').push(this.regsiterForm.value)
+          this.db.database.ref('Users/'+ userData.user.uid).set(this.regsiterForm.value)
+          console.log(userData)
+        }).catch(e=>
+          {
+            this.registerAlert('Error', e.message)
+            console.log(e)
+          })
+    }
+    
+  }
+
+  async registerAlert(status, sms) {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: status,
+      subHeader: sms,
+      buttons: ['OK']
+    });
+
+    await alert.present();
   }
 
 }
