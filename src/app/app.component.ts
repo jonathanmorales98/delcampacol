fd9d4a3299d13b18f1e88abe6c2faf0795a7215d
jnam <onik.1899@gmail.com>
2021-04-06 20:42:51 -0500

    añadi el servicio de usuario

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { Router } from '@angular/router';
+import { UserService } from './services/user.service';
 
 @Component({
   selector: 'app-root',
@@ -8,13 +9,13 @@ import { Router } from '@angular/router';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor(private auth:AngularFireAuth, private router:Router) 
+  constructor(private auth:AngularFireAuth, private router:Router, private user:UserService) 
   {
     this.verifyCurrentUser()
   }
 
   verifyCurrentUser(){
-    this.auth.authState.subscribe(e=>
+    this.auth.authState.subscribe((e:any)=>
       {
         console.log(e)
         if(e == null)
@@ -23,6 +24,8 @@ export class AppComponent {
         }
         else
         {
+          this.user.setUid(e.uid)
+          localStorage.setItem("uid", e.uid)
           this.router.navigate(['/tabs/noticias'], {replaceUrl:true}) //el replace es para que no se regrese con las flechas del celular
         }
         
