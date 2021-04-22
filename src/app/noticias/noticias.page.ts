217e5e28f927f45138f870fcfb6fe6cb50e85a54
jnam <onik.1899@gmail.com>
2021-04-22 13:10:11 -0500

    cargar y ver noticias

--- src/app/noticias/noticias.page.ts
+++ src/app/noticias/noticias.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../services/user.service';
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
 
 @Component({
   selector: 'app-noticias',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NoticiasPage implements OnInit {
 
-  constructor() { }
+  news
+  array = []
+
+  constructor(private user:UserService, private iab:InAppBrowser) { }
 
   ngOnInit() {
+    
+    this.user.getNews().subscribe(News=>
+    {
+      this.news = News;  //guarda el Object traido en un Object local
+      console.log(News)
+      console.log(this.news)
+      this.array = Object.values(this.news.articles); //el object local se convierteen Array  y se guarda en uno
+      //this.array = Object.values(this.news.data); //muy desactualizado
+      console.log("aaaaaaaa"+this.array)
+    })
+
+    
+  }
+
+  openURL(url)
+  {
+    console.log(url)
+    this.iab.create(url)
   }
 
 }
