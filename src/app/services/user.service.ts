701e192afaca804be263b0bb07832b4a84b5069c
jnam <onik.1899@gmail.com>
2021-04-22 13:08:29 -0500

    Api Noticias

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { NGSP_UNICODE } from '@angular/compiler';
 import { Injectable } from '@angular/core';
 
@@ -8,16 +9,28 @@ export class UserService {
 
   uid:string
 
-  constructor() {}
+  
+
+
+  constructor(private http:HttpClient) {}
 
   setUid(_uid:string)
   {
     this.uid = _uid
+    console.log("ususario: ")
     console.log(_uid)
   }
 
   getUid()
   {
     return this.uid
+  }  
+
+  getNews()
+  {
+    return this.http.get("https://newsapi.org/v2/top-headlines?country=co&category=health&apiKey=c89e6b4e04d14b0ab60233d757a5d3eb")
+    //return this.http.get("http://api.mediastack.com/v1/news?access_key=a84f80f4d0a65521b460ab20b2f6634a&countries=co") //muy desactualizado
+    //return this.http.get("https://randomuser.me/api/?results=5
+    
   }
 }
