180a79d5945ee2de3973c82fff178191d47c05f0
jnam <onik.1899@gmail.com>
2021-04-22 19:06:58 -0500

    se guarda en una variable el tipo de cosechador

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
-import { Router } from '@angular/router';
+import { AngularFireDatabase } from '@angular/fire/database';
+import { ActivatedRoute, Router } from '@angular/router';
 import { UserService } from './services/user.service';
 
 @Component({
@@ -9,13 +10,29 @@ import { UserService } from './services/user.service';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor(private auth:AngularFireAuth, private router:Router, private user:UserService) 
+
+  type: string='';
+  uid:string;
+
+  constructor(private auth:AngularFireAuth, private router:Router, private user:UserService, private active:ActivatedRoute, private db:AngularFireDatabase) 
   {
+    this.uid = localStorage.getItem('uid')
+
+    this.db.database.ref('Users/'+this.uid).once('value', (snapshot)=>{
+      //console.log("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaxd");
+      //console.log(snapshot.val());
+      this.type = snapshot.val().type
+      console.log("tipo de ususario", this.type);
+
+    })
+
     this.verifyCurrentUser()
+
   }
+  
 
   verifyCurrentUser(){
-    this.auth.authState.subscribe((e:any)=>
+    this.auth.authState.subscribe((e:any)=>        // es para validar si hay un usuario logeado en el dispositivo
       {
         console.log(e)
         if(e == null)
@@ -26,7 +43,11 @@ export class AppComponent {
         {
           this.user.setUid(e.uid)
           localStorage.setItem("uid", e.uid)
-          this.router.navigate(['/tabs/noticias'], {replaceUrl:true}) //el replace es para que no se regrese con las flechas del celular
+          if(this.type == "Cosechador")
+          {
+            this.router.navigate(['/index-cosechador'], {replaceUrl:true})
+          }
+          //this.router.navigate(['/tabs/noticias'], {replaceUrl:true}) //el replace es para que no se regrese con las flechas del celular
         }
         
       })
