2e99519d78e9a822203d7d62dc5113d49f7ff179
jnam <onik.1899@gmail.com>
2021-05-11 16:17:20 -0500

    ver, actualizar y eliminar publicaciones

--- src/app/publicaciones/publicaciones.page.ts
+++ src/app/publicaciones/publicaciones.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/database';
+import { AlertController } from '@ionic/angular';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-publicaciones',
@@ -6,10 +9,109 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./publicaciones.page.scss'],
 })
 export class PublicacionesPage implements OnInit {
+  itemRef: any;
+  myProducts = [];
+  uid: string;
 
-  constructor() { }
+  file:string;
+
+
+  constructor(private db: AngularFireDatabase, private user:UserService, private alert:AlertController) { 
+    this.uid = user.getUid();
+  }
 
   ngOnInit() {
+    this.itemRef = this.db.object('Cultivo/' + this.uid);
+      this.itemRef.snapshotChanges().subscribe(action => 
+      {
+        console.log(action.type);
+        console.log(action.key)
+        console.log(action.payload.val())
+        let data = action.payload.val()
+        this.myProducts = [];
+        for(let i in data)
+        {
+          let user = data[i];
+          user.key = i
+          console.log(user, i);
+          this.myProducts.push(user)
+        }
+      });
+  }
+
+  //----------------------------------------------esta funcion era una alerta al oprimir la card
+  // async options(type, name) {
+  //   const alert = await this.alert.create({
+  //     cssClass: 'my-custom-class',
+  //     header: 'Confirm!',
+  //     message: 'Message <strong>text</strong>!!!',
+  //     buttons: [
+  //       {
+  //         text: 'Cancelar',
+  //         role: 'cancel',
+  //         cssClass: 'secondary',
+  //         handler: (blah) => {
+  //           console.log('Confirm Cancel: blah');
+  //         }
+  //       },
+  //       {
+  //         text: 'Editar',
+  //         //role: 'cancel',
+  //         cssClass: 'secondary',
+  //         handler: (blah) => {
+  //           //console.log('Confirm Cancel: blah');
+  //         }
+  //       }, {
+  //         text: 'Eliminar',
+  //         cssClass: 'danger',
+  //         handler: () => {
+  //           this.delete(type, name)
+  //           //console.log('Confirm Okay');
+  //         }
+  //       }
+  //     ]
+  //   });
+  //   await alert.present();
+  // }
+
+  async delete(type, name)
+  {
+    console.log("Eliminado...");
+    console.log(type, name);
+    
+    const alert = await this.alert.create({
+      header: 'Espera!',
+      message: 'Este producto se eliminara de tu lista. ¿Quieres continuar?',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: (blah) => {
+            console.log('Confirm Cancel: blah');
+          }
+        }, {
+          text: 'Eliminar',
+          handler: () => {
+            console.log('Confirm Okay');
+            console.log(type);
+            this.db.database.ref(type + '/' + this.uid + '/' + name).remove();
+            this.user.removeImage(this.file, type, name);
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+  
+
+  edit()
+  {
+    console.log("Editando...");
   }
 
 }
+
+// 04/05/2021
+// hice el llado de la base de datos y lo guarde en un array para poder  recorrerlo
